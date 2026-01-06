983e6f484087b907fde2ba42678d733d5eee531d
Jade Westover <jade424433@gmail.com>
2026-01-05 20:30:32 -0800

    add frameworks

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_local_llm/flutter_local_llm_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_local_llm_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterLocalLlmPlugin");
+  flutter_local_llm_plugin_register_with_registrar(flutter_local_llm_registrar);
+}
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_local_llm
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalLlmPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalLlmPlugin"))
+}
